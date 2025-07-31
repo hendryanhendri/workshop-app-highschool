0dcd0783892fd450821f6face6ed7e1fce02ee43
Hendri Sudianto <hendri@go-jek.com>
2025-07-31 19:44:02 +0700

    update name on footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer className="w-full bg-gray-100 text-gray-500 text-center py-4 border-t fixed bottom-0 left-0 right-0 z-40">
             <span>&copy; {new Date().getFullYear()} Alquran App. All rights reserved.</span>
             <span className="block mt-1">
-                develop by <a href="https://hendrisudianto.my.id/" target="_blank" rel="noopener noreferrer" className="underline text-blue-600">https://hendrisudianto.my.id/</a>
+                develop by <a href="https://hendrisudianto.my.id/" target="_blank" rel="noopener noreferrer" className="underline text-blue-600">Hendri Sudianto</a>
             </span>
         </footer>
     );
